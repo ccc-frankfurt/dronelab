e3dcf7b9479b0c03fc5adba92f862000605f81ad
codehering <64325723+codehering@users.noreply.github.com>
2021-06-10 09:42:29 +0200

    Update rsync_to_gateway.sh
    added pw parameter

--- rsync_to_gateway.sh
+++ rsync_to_gateway.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+#set password parameter
+set -e
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Require password'
+    exit 1
+fi
+
+pw=$1
 TARGET=/data/AISEL/dronelab
 SOURCE=/home/pi/data
 GATEWAYIP=141.2.10.184
 DRONENAME=TELLO-588A0C
 
 
+
+
 # sync pictures to remote server
-ssh pi@$GATEWAYIP mkdir -p $TARGET/$DRONENAME
+sshpass $pw -p ssh pi@$GATEWAYIP mkdir -p $TARGET/$DRONENAME
 rsync --relative --inplace --partial --append --progress -avz $SOURCE/./$DRONENAME/ pi@$GATEWAYIP:$TARGET/
 
