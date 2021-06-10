65ecbb22f29abcb7584e35c98c251376fb2007b7
mrammens <mrammens@aigrow.de>
2021-06-10 10:33:32 +0200

    rsync final integration

--- rsync_to_gateway.sh
+++ rsync_to_gateway.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 #set password parameter
-set -e
-
-if [[ $# -eq 0 ]] ; then
-    echo 'Require password'
-    exit 1
-fi
-
-pw=$1
 TARGET=/data/AISEL/dronelab
 SOURCE=/home/pi/data
 GATEWAYIP=141.2.10.184
@@ -17,6 +9,6 @@ DRONENAME=TELLO-588A0C
 
 
 # sync pictures to remote server
-sshpass $pw -p ssh pi@$GATEWAYIP mkdir -p $TARGET/$DRONENAME
+ssh pi@$GATEWAYIP mkdir -p $TARGET/$DRONENAME
 rsync --relative --inplace --partial --append --progress -avz $SOURCE/./$DRONENAME/ pi@$GATEWAYIP:$TARGET/
 
