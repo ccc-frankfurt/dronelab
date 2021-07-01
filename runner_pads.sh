00f4a39e06af071cbece301ddd31d70d402d2c85
5yntek <Xyntek@t-online.de>
2021-07-01 12:06:46 +0200

    removed flag ops_mode in pad run

--- runner_pads.sh
+++ runner_pads.sh
@@ -1,1 +1,1 @@
-LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1 /usr/bin/python3 /home/pi/dronelab/runDrone.py --move_mode fixroutepads --save_stream --save_meta --ops_mode
+LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1 /usr/bin/python3 /home/pi/dronelab/runDrone.py --move_mode fixroutepads --save_stream --save_meta
